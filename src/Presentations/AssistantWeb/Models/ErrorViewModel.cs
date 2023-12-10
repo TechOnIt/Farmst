898cf048821f2269d70a0bd3702816c28bd6af5b
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-12-10 22:28:05 +0330

    🌱 Initialize architecture.

---
+++ src/Presentations/AssistantWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AssistantWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
