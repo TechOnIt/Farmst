adb4e476c6bd22aa1659b49881ced5642cadaff3
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-12-12 19:59:23 +0330

    🌱 Initialize bases of entities.

---
+++ src/Libraries/Domain/Entities/Catalogs/CategoryEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Entities.Catalogs
+{
+    public class CategoryEntity : BaseEntity
+    {
+        #region Ctor
+
+        CategoryEntity() { }
+        public CategoryEntity(string name)
+        {
+            Name = name;
+        }
+
+        #endregion
+
+        public string Name { get; set; } = string.Empty;
+
+        public Guid? ParentId { get; set; }
+        public virtual CategoryEntity? Parent { get; set; }
+    }
+}
---
+++ src/Libraries/Domain/Entities/Catalogs/PlantEntity.cs
@@ -0,0 +1,29 @@
+﻿namespace Domain.Entities.Catalogs
+{
+    public class PlantEntity : BaseEntity
+    {
+        #region Ctor
+
+        PlantEntity() { }
+        public PlantEntity(string name)
+        {
+            SetName(name);
+        }
+
+        #endregion
+
+        public string Name { get; private set; } = string.Empty;
+        public string? Description { get; set; }
+
+        #region Methods
+
+        public void SetName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException("name cannot be null.");
+            Name = name;
+        }
+
+        #endregion
+    }
+}
